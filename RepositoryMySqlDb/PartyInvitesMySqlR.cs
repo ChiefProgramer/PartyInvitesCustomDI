ee3bb811ecb192ac770ad1765d9e2a952ec1abed
Joseph Wilson <josephawilson86@gmail.com>
2018-02-08 17:36:16 -0800

    still working on the mysql connection

---
+++ RepositoryMySqlDb/PartyInvitesMySqlR.cs
@@ -0,0 +1,43 @@
+﻿namespace RepositoryMySqlDb
+{
+    using System;
+    using System.Collections.Generic;
+    using Contracts;
+    using Entities;
+    using Dapper;
+
+    class PartyInvitesMySqlR : IPartyInvitesR
+    {
+
+        public void Add(GuestResponse aGuestResponse)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int aGuesResponseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public GuestResponse Get(int aGuestResponseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<GuestResponse> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(GuestResponse aGuestResponse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
