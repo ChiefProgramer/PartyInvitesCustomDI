451f5d3ebe7ab052b63f563504255e0e869b24a4
ChiefProgramer <rgenware@gmail.com>
2018-02-24 23:06:00 -0700

    Added Rex's Tests

--- RepositoryMemory.Test/TestRepositoryMemory.cs
+++ RepositoryMemory.Test/TestRepositoryMemory.cs
@@ -1,40 +1,43 @@
 namespace RepositoryMemory.Test
 {
-	using Contracts;
-	using Entities;
-	using FluentAssertions;
-	using Xunit;
+    using Contracts;
+    using Entities;
+    using FluentAssertions;
+    using Xunit;
 
-	public class TestRepositoryMemory
-	{
-		private GuestResponse MakeNewRecord()
-		{
-			GuestResponse vResult = new GuestResponse
-			{
-				Email = "Some@Email.com"
-				, Name = "Some Name"
-				, Phone = "123-4567"
-				, WillAttend = true
-			};
-			return vResult;
-		}
+    public class TestRepositoryMemory
+    {
+        private Guest MakeNewRecord()
+        {
+            Guest vResult = new Guest
+            {
+                Email = "Some@Email.com"
+                ,
+                Name = "Some Name"
+                ,
+                Phone = "123-4567"
+                ,
+                WillAttend = true
+            };
+            return vResult;
+        }
 
-		[Fact]
-		public void TestAdd()
-		{
-			// Arrange
-			int vExpected = 1;
-			IGuestR vRepository = new PartyInvitesR();
-			int vCount = vRepository.Count();
-			GuestResponse vRecord = MakeNewRecord();
+        [Fact]
+        public void TestAdd()
+        {
+            // Arrange
+            int vExpected = 1;
+            IGuestR vRepository = new GuestRepositoryMemory();
+            int vCount = vRepository.Count();
+            Guest vRecord = MakeNewRecord();
 
-			// Act
-			vRepository.Add(vRecord);
-			int vResult = vRepository.Count();
+            // Act
+            vRepository.Add(vRecord);
+            int vResult = vRepository.Count();
 
-			// Assert
-			vResult.Should().Be(vExpected);
-		}
+            // Assert
+            vResult.Should().Be(vExpected);
+        }
 
-	}
+    }
 }
